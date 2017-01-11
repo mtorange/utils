5bf7aacaab6357deb9b306a0b8224a8d3cb03367
mtorange <mtorange@users.noreply.github.com>
2017-01-11 14:51:39 +0900

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -2,7 +2,7 @@
 
 function usage 
 {
-	echo 'Usage : ' $0 'target-file' ' [args]'
+	echo 'Usage : ' $0 'target-file-to-be-tested' '[command-to-be-executed arguments...]'
 	echo ""
 	exit -1
 }
@@ -16,28 +16,73 @@ fi
 
 shift
 
-wheel=('-'  '\'  '|'  '/')
-i=0
-executed_mtime=00000000000
+if [ $# -ge 1 ]; then
+	CMD=$1
+	shift
+else
+	CMD=$TARGET
+fi
 
+wheel=('-'  '\'  '|'  '/')
+wheel_color=(31 32 33 34 35 36 37)
+i=0
+j=0
+default_mtime=00000000000
+executed_mtime=$default_mtime
+options=()
+
+OS=$(uname -s)
+case $OS in
+	(Darwin)
+		options=(-q -f "%m" "$TARGET")
+		;;
+	(Linux)
+		options=(-c "%Y" "$TARGET")
+		;;
+	(CYGWIN*)
+		options=(-c "%Y" "$TARGET")
+		;;
+	(*)
+		echo "Cannot detect OS"
+		exit
+esac
+
+echo "-------["$0"]-------------"
+echo "-- Monitoring command     : 'stat "${options[*]}"'"
+echo "-- Command to be executed : '"$CMD $*"'"
 while true
 do
-	mtime=`stat -f "%m" $TARGET`
-	if [ $executed_mtime != $mtime ]; then
+	mtime=$(stat ${options[*]} || echo $default_mtime)
+
+	if [ \( "L"$executed_mtime != "L"$mtime \)  -a \( "L"$mtime != "L"$default_mtime \) ]; then
 		echo
 		echo === START =============================
-		echo EXEC: $TARGET $*
+		echo EXEC: $CMD $*
 		echo .......................................
-		$TARGET $*
+		$CMD "$@"
 		echo .......................................
 		echo 
 		executed_mtime=$mtime
-	else
-		for a in 1 2 3 4 5 6 7 8 9 0 ; do
-			let "i = ($i + 1) % 4"
-			echo -n -e "\r"
-			echo -n  ${wheel[$i]} 
-			sleep 0.1
-		done
 	fi
+
+	let "i = ($i + 1) % ${#wheel[*]}"
+	let "j = ($j + 1) % ${#wheel_color[*]}"
+	echo -n -e "\r"
+	echo -n  "[${wheel_color[$j]}m"${wheel[$i]}"[0m"
+	if /usr/bin/read -t 1; then
+		executed_mtime=00000000000000000
+	fi
+
+#		for a in 1 2 3 4 5 ; do
+#			let "i = ($i + 1) % ${#wheel[*]}"
+#			let "j = ($j + 1) % ${#wheel_color[*]}"
+#			echo -n -e "\r"
+#			echo -n  "[${wheel_color[$j]}m"${wheel[$i]}"[0m"
+#			#sleep 0.2
+#			if /usr/bin/read -t 1; then
+#				executed_mtime=00000000000000000
+#				echo "ENTER"
+#				break
+#			fi
+#		done
 done
